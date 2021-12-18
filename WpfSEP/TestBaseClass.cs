6acf883f16959732e3aa990d15db334b63c805f5
AN VÕ THIỆN <voan0935995700@gmail.com>
2021-12-18 13:55:02 +0700

    initial

---
+++ WpfSEP/TestBaseClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace WpfSEP
+{
+    public class Student {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+    public partial class TestBaseClass: Window
+    {
+        public TestBaseClass(): base(){}
+    }
+
+    public partial class TestBaseClassGeneric<T> : TestBaseClass
+    {
+        public List<T> Datas { get; set; }
+
+        private void SetupData(IEnumerable<T> sourceData)
+        {
+            this.Datas = sourceData.ToList();
+        }
+        
+        public TestBaseClassGeneric(): base()
+        {
+            
+        }
+    }
+}
