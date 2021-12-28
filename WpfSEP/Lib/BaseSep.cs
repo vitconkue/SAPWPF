4d190e2cb59bfe141f920dfed91206fe627d0c8d
AN VÕ THIỆN <voan0935995700@gmail.com>
2021-12-28 12:27:11 +0700

    proof of concept

---
+++ WpfSEP/Lib/BaseSep.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfSEP.Lib
+{
+    public class Student {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BaseSep: Window
+    {
+        protected BaseSep() : base()
+        {
+        }
+    }
+
+    public class BaseSepGeneric<T> : BaseSep
+    {
+        public SepUserControlGeneric<T> SepUserControl = new SepUserControlGeneric<T>();
+        public List<T> Datas { get; set; } = new List<T>();
+
+        protected void SetupData(IEnumerable<T> sourceData)
+        {
+            this.Datas = sourceData.ToList();
+            this.SepUserControl.SetupData(this.Datas);
+        }
+        
+        public BaseSepGeneric()
+        {
+            
+        }
+        
+    }
+}
---
+++ WpfSEP/Lib/SepUserControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfSEP.Lib
+{
+    public partial class SepUserControl : UserControl
+    {
+
+        public SepUserControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+
+    public class SepUserControlGeneric<T> : SepUserControl
+    {
+        public List<T> Datas { get; set; }
+        public ObservableCollection<T> ObservableCollection { get; set; } = new ObservableCollection<T>();
+
+        public SepUserControlGeneric()
+        {
+            DataGrid.ItemsSource = ObservableCollection;
+        }
+
+        public void SetupData(List<T> source)
+        {
+            Datas = source;
+            foreach (var data in Datas)
+            {
+                ObservableCollection.Add(data);
+            }
+        }
+    }
+}
