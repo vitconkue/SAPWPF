93d530527f501efe9d4a6e649aba586a8486cb0f
AN VÕ THIỆN <voan0935995700@gmail.com>
2021-12-28 12:36:12 +0700

    add files

---
+++ WpfLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows;
+
+namespace WpfLibrary1
+{
+    public class Test: Window
+    {
+        
+    }
+}
---
+++ WpfSEP/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfSEP.Lib;
+
+namespace WpfSEP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : BaseSepGeneric<Student>
+    {
+        public MainWindow(): base()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Setup()
+        {
+            List<Student> datas = new List<Student>();
+            datas.Add(new Student{Name = "An", Id = "1"});
+            datas.Add(new Student{Name = "Hoa", Id = "2"});
+            SetupData(datas);
+            this.StackPanel.Children.Add(SepUserControl); 
+        }
+
+
+        private void OpenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Setup();
+        }
+    }
+}
